d8e208187d9e5591512c0ac162e8b0564535bbc0
Rafa Mel <melrcm@gmx.net>
2020-07-28 13:07:05 +0200

    docs: adds UML diagram for incoming pull implementation

--- kpo.scripts.js
+++ kpo.scripts.js
@@ -3,9 +3,28 @@ const { scripts } = require('./project.config');
 
 module.exports.scripts = {
   ...scripts,
-  build: [scripts.build, kpo.series('npm pack', { cwd: './pkg' })],
+  build: [
+    scripts.build,
+    kpo.json('./pkg/package.json', ({ json }) => ({
+      ...json,
+      files: [...json.files, 'operators/']
+    })),
+    kpo.json('./pkg/react/package.json', () => ({
+      sideEffects: false,
+      name: 'multitude/operators',
+      main: '../dist/operators/index.js',
+      types: '../dist/operators/index.d.ts',
+      esnext: '../dist-src/operators/index.js'
+    })),
+    kpo.series('npm pack', { cwd: './pkg' })
+  ],
   watch: 'onchange ./src --initial --kill -- kpo watch:task',
   'watch:test': 'kpo test -- --watch',
+  docs: [scripts.docs, kpo.kpo`docs:uml`],
+  'docs:uml': [
+    'puml generate assets/Definitions.puml -o assets/Definitions.png',
+    'puml generate assets/Consume.puml -o assets/Consume.png'
+  ],
 
   /* Private */
   ['$watch:task']: [kpo.log`\x1Bc⚡`, 'kpo lint build']
