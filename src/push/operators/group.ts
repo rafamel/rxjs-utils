87e66ac7b98eeacc71f655b81076119b5ed722d9
Rafa Mel <melrcm@gmx.net>
2020-11-21 08:21:33 +0100

    feat(push): adds group operator

---
+++ src/push/operators/group.ts
@@ -0,0 +1,26 @@
+import { Push } from '@definitions';
+import { TypeGuard } from '@helpers';
+import { operate } from '../utils';
+
+/**
+ *
+ * @param every default: 2
+ */
+export function group<T>(every?: number): Push.Operation<T, T[]> {
+  const number = TypeGuard.isEmpty(every) ? 2 : every;
+
+  return operate<T, T[]>((tb) => {
+    let arr: T[] = [];
+
+    return {
+      next(value: T): void {
+        arr.push(value);
+        if (arr.length >= number) {
+          const response = arr;
+          arr = [];
+          tb.next(response);
+        }
+      }
+    };
+  });
+}
---
+++ test/push/operators/group.test.ts
@@ -0,0 +1,50 @@
+import { Observable, PushStream, group } from '@push';
+import { into } from 'pipettes';
+import assert from 'assert';
+
+test(`returns PushStream`, () => {
+  const obs = into(new Observable(() => undefined), group());
+  assert(obs instanceof PushStream);
+});
+test(`succeeds w/ default`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+    }),
+    group()
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [
+    [1, 2],
+    [3, 4]
+  ]);
+});
+test(`succeeds w/ custom`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+      obs.next(7);
+    }),
+    group(3)
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [
+    [1, 2, 3],
+    [4, 5, 6]
+  ]);
+});
