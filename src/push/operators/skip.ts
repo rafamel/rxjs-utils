5045ec06c4157ca485bf25f41571a3577d117ba8
Rafa Mel <melrcm@gmx.net>
2020-11-21 08:07:54 +0100

    feat(push): adds skip operator

---
+++ src/push/operators/skip.ts
@@ -0,0 +1,29 @@
+import { Push } from '@definitions';
+import { TypeGuard } from '@helpers';
+import { operate } from '../utils';
+
+export interface SkipOptions<T> {
+  count?: number;
+  while?: (value: T, index: number) => boolean;
+}
+
+export function skip<T>(count: number | SkipOptions<T>): Push.Operation<T> {
+  const options = !count || TypeGuard.isNumber(count) ? { count } : count;
+
+  return operate<T>((tb) => {
+    let index = -1;
+    let stop = false;
+    return {
+      next(value: T): void {
+        index++;
+
+        if (stop) return tb.next(value);
+        if (options.count && index < options.count) return;
+        if (options.while && options.while(value, index)) return;
+
+        stop = true;
+        return tb.next(value);
+      }
+    };
+  });
+}
---
+++ test/push/operators/skip.test.ts
@@ -0,0 +1,122 @@
+import { Observable, PushStream, skip } from '@push';
+import { into } from 'pipettes';
+import assert from 'assert';
+
+test(`returns PushStream`, () => {
+  const obs = into(new Observable(() => undefined), skip(0));
+  assert(obs instanceof PushStream);
+});
+test(`succeeds w/ count (1)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+    }),
+    skip(3)
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [4, 5, 6]);
+});
+test(`succeeds w/ count (2)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+    }),
+    skip({ count: 3 })
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [4, 5, 6]);
+});
+test(`succeeds w/ while (value)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+      obs.next(2);
+      obs.next(1);
+    }),
+    skip({ while: (x: number) => x < 3 })
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [3, 4, 5, 6, 2, 1]);
+});
+test(`succeeds w/ while (index)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+    }),
+    skip({ while: (_: any, i) => i < 3 })
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [4, 5, 6]);
+});
+test(`succeeds w/ count + while (1)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+      obs.next(2);
+      obs.next(1);
+    }),
+    skip({ count: 2, while: (x: number) => x < 5 })
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [5, 6, 2, 1]);
+});
+test(`succeeds w/ count + while (2)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+      obs.next(2);
+      obs.next(1);
+    }),
+    skip({ count: 4, while: (x: number) => x < 2 })
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [5, 6, 2, 1]);
+});
