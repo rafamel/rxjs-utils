cedf7d4aa0a9736a79aeaf01cfaec1b99477e85c
Rafa Mel <melrcm@gmx.net>
2020-11-21 08:08:25 +0100

    feat(push): adds take operator

---
+++ src/push/operators/take.ts
@@ -0,0 +1,33 @@
+import { Push } from '@definitions';
+import { TypeGuard } from '@helpers';
+import { operate } from '../utils';
+
+export interface TakeOptions<T> {
+  count?: number;
+  while?: (value: T, index: number) => boolean;
+}
+
+export function take<T>(count: number | TakeOptions<T>): Push.Operation<T> {
+  const options = !count || TypeGuard.isNumber(count) ? { count } : count;
+
+  return operate<T>((tb) => {
+    let index = -1;
+    let stop = false;
+    return {
+      next(value: T): void {
+        if (stop) return;
+
+        index++;
+        if (options.count && index < options.count) {
+          return tb.next(value);
+        }
+        if (options.while && options.while(value, index)) {
+          return tb.next(value);
+        }
+
+        stop = true;
+        return tb.complete();
+      }
+    };
+  });
+}
---
+++ test/push/operators/take.test.ts
@@ -0,0 +1,128 @@
+import { Observable, PushStream, take } from '@push';
+import { into } from 'pipettes';
+import assert from 'assert';
+
+test(`returns PushStream`, () => {
+  const obs = into(new Observable(() => undefined), take(0));
+  assert(obs instanceof PushStream);
+});
+test(`succeeds w/ count (1)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+    }),
+    take(3)
+  );
+
+  const values: any[] = [];
+  const broker = obs.subscribe((x) => values.push(x));
+
+  assert(broker.closed);
+  assert.deepStrictEqual(values, [1, 2, 3]);
+});
+test(`succeeds w/ count (2)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+    }),
+    take({ count: 3 })
+  );
+
+  const values: any[] = [];
+  const broker = obs.subscribe((x) => values.push(x));
+
+  assert(broker.closed);
+  assert.deepStrictEqual(values, [1, 2, 3]);
+});
+test(`succeeds w/ while (value)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+      obs.next(2);
+      obs.next(1);
+    }),
+    take({ while: (x: number) => x < 3 })
+  );
+
+  const values: any[] = [];
+  const broker = obs.subscribe((x) => values.push(x));
+
+  assert(broker.closed);
+  assert.deepStrictEqual(values, [1, 2]);
+});
+test(`succeeds w/ while (index)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+    }),
+    take({ while: (_: any, i) => i < 3 })
+  );
+
+  const values: any[] = [];
+  const broker = obs.subscribe((x) => values.push(x));
+
+  assert(broker.closed);
+  assert.deepStrictEqual(values, [1, 2, 3]);
+});
+test(`succeeds w/ count + while (1)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+      obs.next(2);
+      obs.next(1);
+    }),
+    take({ count: 2, while: (x: number) => x < 5 })
+  );
+
+  const values: any[] = [];
+  const broker = obs.subscribe((x) => values.push(x));
+
+  assert(broker.closed);
+  assert.deepStrictEqual(values, [1, 2, 3, 4]);
+});
+test(`succeeds w/ count + while (2)`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+      obs.next(2);
+      obs.next(1);
+    }),
+    take({ count: 4, while: (x: number) => x < 2 })
+  );
+
+  const values: any[] = [];
+  const broker = obs.subscribe((x) => values.push(x));
+
+  assert(broker.closed);
+  assert.deepStrictEqual(values, [1, 2, 3, 4]);
+});
