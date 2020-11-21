308d6953047c91cdb9016cf711640bd65449413d
Rafa Mel <melrcm@gmx.net>
2020-11-21 08:23:33 +0100

    feat(push): adds trail operator

---
+++ src/push/operators/trail.ts
@@ -0,0 +1,24 @@
+import { Push } from '@definitions';
+import { TypeGuard } from '@helpers';
+import { operate } from '../utils';
+
+/**
+ *
+ * @param count default: 2
+ */
+export function trail<T>(count?: number): Push.Operation<T, T[]> {
+  const number = TypeGuard.isEmpty(count) ? 2 : count;
+
+  return operate<T, T[]>((tb) => {
+    const arr: T[] = [];
+    return {
+      next(value: T): void {
+        arr.push(value);
+        if (arr.length > number) arr.shift();
+        if (arr.length === number) {
+          tb.next(Array.from(arr));
+        }
+      }
+    };
+  });
+}
---
+++ test/push/operators/trail.test.ts
@@ -0,0 +1,55 @@
+import { Observable, PushStream, trail } from '@push';
+import { into } from 'pipettes';
+import assert from 'assert';
+
+test(`returns PushStream`, () => {
+  const obs = into(new Observable(() => undefined), trail());
+  assert(obs instanceof PushStream);
+});
+test(`succeeds w/ default`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+    }),
+    trail()
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [4, 5]
+  ]);
+});
+test(`succeeds w/ custom`, () => {
+  const obs = into(
+    new PushStream<number>((obs) => {
+      obs.next(1);
+      obs.next(2);
+      obs.next(3);
+      obs.next(4);
+      obs.next(5);
+      obs.next(6);
+      obs.next(7);
+    }),
+    trail(3)
+  );
+
+  const values: any[] = [];
+  obs.subscribe((x) => values.push(x));
+
+  assert.deepStrictEqual(values, [
+    [1, 2, 3],
+    [2, 3, 4],
+    [3, 4, 5],
+    [4, 5, 6],
+    [5, 6, 7]
+  ]);
+});
