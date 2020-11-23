ea38253eca5290f0d63ca4a75896cf7f044fce23
Rafa Mel <melrcm@gmx.net>
2020-11-23 12:35:10 +0100

    feat(push): from creates from PromiseLike

--- src/push/creation/from.ts
+++ src/push/creation/from.ts
@@ -1,14 +1,41 @@
 import { Push } from '@definitions';
+import { TypeGuard } from '@helpers';
 import { Observable, PushStream } from '../streams';
-import { isObservableLike } from '../utils';
+import { isObservableCompatible, isObservableLike } from '../utils';
+import { of } from './of';
 
 export function from<T>(
   this: Push.StreamConstructor | void,
-  item: Push.Observable<T> | Push.Compatible<T> | Push.Like<T> | Iterable<T>
+  item:
+    | Push.Observable<T>
+    | Push.Compatible<T>
+    | Push.Like<T>
+    | Iterable<T>
+    | PromiseLike<T>
 ): Push.Stream<T> {
   const from = Observable.from.bind(PushStream);
 
-  return isObservableLike(item)
-    ? from({ [Symbol.observable]: () => item as any })
-    : from(item);
+  if (isObservableCompatible(item)) {
+    return from(item);
+  }
+  if (isObservableLike(item)) {
+    return from({ [Symbol.observable]: () => item as any });
+  }
+  if (TypeGuard.isIterable(item)) {
+    return of(...item);
+  }
+  if (TypeGuard.isPromiseLike(item)) {
+    return new PushStream((obs) => {
+      item.then(
+        (value) => {
+          obs.next(value);
+          obs.complete();
+        },
+        (error) => {
+          obs.error(error);
+        }
+      );
+    });
+  }
+  throw new TypeError(`Unable to convert ${typeof item} into a PushStream`);
 }
